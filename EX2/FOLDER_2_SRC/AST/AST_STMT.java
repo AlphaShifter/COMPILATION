a659e29527dded87b5b7c20c7cc3d5e41832dab8
Ohad <iohadrubin@gmail.com>
2017-11-20 19:51:52 +0200

    Added more rules, cField list, more declarations.

--- EX2/FOLDER_2_SRC/AST/AST_STMT.java
+++ EX2/FOLDER_2_SRC/AST/AST_STMT.java
@@ -5,6 +5,7 @@ public abstract class AST_STMT extends AST_Node
 	/*********************************************************/
 	/* The default message for an unknown AST statement node */
 	/*********************************************************/
+
 	public void PrintMe()
 	{
 		System.out.print("UNKNOWN AST STATEMENT NODE");
