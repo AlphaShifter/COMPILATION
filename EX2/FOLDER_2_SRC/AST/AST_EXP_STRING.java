8e75cd727419e0df5df6931c5a360283cdf962a8
gilad_ilsar <gandismidas1>
2017-11-20 18:09:41 +0200

    add exp derative rules and classes

---
+++ EX2/FOLDER_2_SRC/AST/AST_EXP_STRING.java
@@ -0,0 +1,45 @@
+package AST;
+
+public class AST_EXP_STRING extends AST_EXP
+{
+	public String value;
+
+	/******************/
+	/* CONSTRUCTOR(S) */
+	/******************/
+	public AST_EXP_STRING(String str)
+	{
+		/******************************/
+		/* SET A UNIQUE SERIAL NUMBER */
+		/******************************/
+		SerialNumber = AST_Node_Serial_Number.getFresh();
+
+		/***************************************/
+		/* PRINT CORRESPONDING DERIVATION RULE */
+		/***************************************/
+		System.out.format("====================== exp -> String( %s )\n", str);
+
+		/*******************************/
+		/* COPY INPUT DATA NENBERS ... */
+		/*******************************/
+		this.value = str;
+	}
+
+	/************************************************/
+	/* The printing message for an INT EXP AST node */
+	/************************************************/
+	public void PrintMe()
+	{
+		/*******************************/
+		/* AST NODE TYPE = AST INT EXP */
+		/*******************************/
+		System.out.format("AST NODE STRING( %s )\n",value);
+
+		/*********************************/
+		/* Print to AST GRAPHIZ DOT file */
+		/*********************************/
+		AST_GRAPHVIZ.getInstance().logNode(
+			SerialNumber,
+			String.format("STRING(%s)",value));
+	}
+}
