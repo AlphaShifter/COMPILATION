70464f9d6649a3d4abf6d68676ca388934d64031
GILAD ILSAR <gilad.ilsar@ibm.com>
2018-01-07 10:01:23 +0200

    Save Word debug

--- EX4/FOLDER_2_SRC/MIPS/sir_MIPS_a_lot.java
+++ EX4/FOLDER_2_SRC/MIPS/sir_MIPS_a_lot.java
@@ -59,7 +59,8 @@ public class sir_MIPS_a_lot {
 
     public void loadAddressLocalVar(TEMP dst, int serialLocalVarNum){
         int idx = dst.getSerialNumber();
-        fileWriter.format("\taddi Temp_%d,$fp,%d\n", idx, -serialLocalVarNum * WORD_SIZE);
+        fileWriter.format("\tlw %s, %d(fp)\n",tempToString(dst),-serialLocalVarNum*WORD_SIZE);
+       // fileWriter.format("\taddi Temp_%d,$fp,%d\n", idx, -serialLocalVarNum * WORD_SIZE);
        //   fileWriter.format("\taddi Temp_%d,$sp,%d\n", idx, -serialLocalVarNum * WORD_SIZE);
 
 
