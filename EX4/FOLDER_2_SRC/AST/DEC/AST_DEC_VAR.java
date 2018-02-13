46f8d7c769641fce7862b0232dffd43143ab9377
gilad_ilsar <gandismidas1>
2018-02-13 19:23:46 +0200

    Debug

--- EX4/FOLDER_2_SRC/AST/DEC/AST_DEC_VAR.java
+++ EX4/FOLDER_2_SRC/AST/DEC/AST_DEC_VAR.java
@@ -123,8 +123,12 @@ public class AST_DEC_VAR extends AST_DEC {
             }
             //out the name with it's frequency
             String newName = name + "_" + c;
-            AST_DEC_FUNC.funcLocalVarsCount.put(newName, AST_DEC_FUNC.funcLocalVarsCount.size() + 1);
-            myPlace = AST_DEC_FUNC.funcLocalVarsCount.size();
+            if(AST_DEC_FUNC.funcLocalVarsCount.containsKey(newName)){
+                myPlace = AST_DEC_FUNC.funcLocalVarsCount.get(newName);
+            } else {
+                AST_DEC_FUNC.funcLocalVarsCount.put(newName, AST_DEC_FUNC.funcLocalVarsCount.size() + 1);
+                myPlace = AST_DEC_FUNC.funcLocalVarsCount.size();
+            }
 //            if(AST_DEC_CLASS.classLocalVarsCount != null)
 //                myPlace += AST_DEC_CLASS.classLocalVarsCount.size();
             varKind = VAR_KIND.LOCAL;
