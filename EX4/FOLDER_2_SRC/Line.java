5b87f6609782b2d1bc105db324d68d80236ee305
Unknown <iohadrubin@gmail.com>
2018-02-21 20:19:12 +0200

    Changes to liveness

--- EX4/FOLDER_2_SRC/Line.java
+++ EX4/FOLDER_2_SRC/Line.java
@@ -1,21 +1,38 @@
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.TreeSet;
 public class Line {
     HashSet<Integer> in;
     HashSet<Integer> out;
-    ArrayList<Integer> succ;
+     HashSet<Integer> intag;
+    HashSet<Integer> outtag;
+    TreeSet<Integer> succ;
     ArrayList<Integer> statment;
+    ArrayList<Integer> def;
+    int use;
+    int linecount;
 
-
-    public Line() {
+    public Line(int c,TreeSet<Integer> succsessor) {
         in= new HashSet<Integer>();
         out= new HashSet<Integer>();
+        intag= new HashSet<Integer>();
+        outtag= new HashSet<Integer>();
         statment= new ArrayList<Integer>();
+        def= new ArrayList<Integer>();
+        succ= succsessor;
+        linecount=c;
     }
 
       @Override
     public String toString() {
-        return statment.toString();
+        return linecount+","+statment.toString()+","+succ.toString()+out.toString()+"\n";
+    }
+    public void update() {
+        if(statment.size()>=1) use=statment.get(0);
+        if(statment.size()>=2) def.add(statment.get(1));
+        if(statment.size()>=3) def.add(statment.get(2));
+
+        
     }
 
 }
--- EX4/FOLDER_2_SRC/MatchTest.java
+++ EX4/FOLDER_2_SRC/MatchTest.java
@@ -18,6 +18,7 @@ import java.util.regex.*;
 public class MatchTest {
     public static void main(String[] args)throws IOException, InterruptedException {
         Liveness();
+        // Leader();
 
 
     }
@@ -49,41 +50,175 @@ public class MatchTest {
     // }
 
     public static void Liveness() throws IOException, InterruptedException{
-        Pattern p = Pattern.compile("(?<=Temp_)\\d{1,7}");
+        Pattern tempMatch = Pattern.compile("(?<=Temp_)\\d{1,7}");
+        Pattern branchMatch = Pattern.compile("(beq|blt|ble|bgt|bge|bne|jal|j )");
+        Pattern jrMatch = Pattern.compile("(?<=jr ).*");
+        Pattern jalMatch = Pattern.compile("(?<=jal ).*");
+        Pattern jumpMatch = Pattern.compile("(j |jr |jal).*");
+        // Pattern Match = Pattern.compile("(?<=j ).*");
+        Pattern labelMatch = Pattern.compile(".*(?=:$)");
         ArrayList<Integer> allMatches;
-        ArrayList<Line> lines = new ArrayList<Line>();
-        Line curr;
-        String line;
-         String inputFile = "../FOLDER_5_OUTPUT/MIPS.txt";
+        TreeSet<Integer> succ;
+        TreeSet<Integer> leaderList = new TreeSet<Integer>();
+        TreeMap<String,Integer> labelMap = new TreeMap<String,Integer>();
+        TreeMap<String,Integer> labelToNearestjr = new TreeMap<String,Integer>();
+        // TreeMap<Integer,TreeSet<Integer>> edges = new TreeMap<Integer,TreeSet<Integer>>();
 
+        TreeSet<Integer> jrSet = new TreeSet<Integer>();
+
+        // leaderList.add(1);
+        TreeMap<Integer,Line> lines = new TreeMap<Integer,Line>(Collections.reverseOrder());
+        Line curr;
+        Matcher m;
+        String line;
+
+        String inputFile = "../FOLDER_5_OUTPUT/MIPS.txt";
+        // BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+        // int totalLines = 0;
+        // while (reader.readLine() != null) {
+        //     totalLines++;
+        // }
+        // int[][] edges = new int[totalLines+1][totalLines+1];
+        // reader.close();
+
+        int linecount=1;
         BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
         while ((line = bufferedReader.readLine()) != null) {
-            Matcher m = p.matcher(line);
-            curr = new Line();
-            // allMatches = new ArrayList<String>();
-            while (m.find()) curr.statment.add(Integer.parseInt(m.group()));
-            lines.add(curr);
-        }
-        //reverse list
-        // Collections.reverse(lines);
-        System.out.println(lines);
+            m = jrMatch.matcher(line);
+            if(m.find()){
+                jrSet.add(linecount);
+            }
+            linecount++;
+        } 
 
-        while(true){
-            for(Line l : lines){
-            l.intag=l.in.clone();
-            l.outtag=l.out.clone();
-            HashSet<Integer> tmp=l.out.clone().removeAll(l.def);
-            tmp.addAll(l.use);
-            l.in=tmp;
-            l.out = new HashSet<Integer>();
-            for(Line s: l.succ){
-                l.out.addAll(s.in);
+        linecount=1;
+        bufferedReader = new BufferedReader(new FileReader(inputFile));
+        while ((line = bufferedReader.readLine()) != null) {
+            succ = new TreeSet<Integer>();
+            if(!jumpMatch.matcher(line).find()){
+                succ.add(linecount+1);
+            }
+            m = labelMatch.matcher(line);
+            if(m.find()){
+                String currlabel = m.group();
+                String[] tmp = line.split("(,| )");
+                labelMap.put(currlabel,linecount);
+                labelToNearestjr.put(currlabel,jrSet.ceiling(linecount));
+            }
+            m = tempMatch.matcher(line);
+            curr = new Line(linecount,succ);
+            while (m.find()) curr.statment.add(Integer.parseInt(m.group()));
+            lines.put(linecount,curr);
+            linecount++;
+        }
+
+
+        linecount=1;
+        bufferedReader = new BufferedReader(new FileReader(inputFile));
+        while ((line = bufferedReader.readLine()) != null) {
+            m = branchMatch.matcher(line);
+            if(m.find()){
+            String[] tmp = line.split("(,| )");
+            lines.get(linecount).succ.add(labelMap.get(tmp[tmp.length-1]));
+            m=jalMatch.matcher(line);
+            if(m.find()){
+                String r = m.group();
+                // System.out.println(line+" :="+r);
+                // System.out.println(labelToNearestjr.get(r));
+                lines.get(labelToNearestjr.get(r)).succ.add(linecount+1);
+                // lines.get(labelToNearestjr.get(m.group())).succ.add(linecount+1);
+
                 }
             }
-            for(Line l : lines){
-                if(!l.in.equals(l.intag)) break;
-                if(!l.out.equals(l.outtag)) break;
+            linecount++;
+        }
+
+
+
+
+        //reverse list
+        // Collections.reverse(lines);
+        // System.out.println(leaderList);
+        // System.out.println(labelMap);
+        // System.out.println(lines);
+        // System.out.println(labelToNearestjr);
+        // System.out.println(jrSet);
+        // return;
+        //TODO - calc succsessor and use
+        for(Line l : lines.values()){
+            // System.out.println(l);
+            l.update();
+        } 
+
+
+        // System.exit(0);
+        int counting=0;
+        // Collections.reverseOrder(lines);
+
+        // new TreeMap<Integer,Line>(Collections.reverseOrder()).
+        // ArrayList<Line> linesList = new ArrayList<Line>(lines.values());
+        // lines=(TreeMap<Integer,Line>)lines.descendingMap();
+        while(true){
+            boolean sent=true;
+            for(Line l : lines.values()){
+                l.intag=new HashSet<Integer>(l.in);
+                l.outtag=new HashSet<Integer>(l.out);
+                HashSet<Integer> tmp=(new HashSet<Integer>(l.out));
+                tmp.removeAll(l.def);
+                tmp.add(l.use);
+                l.in=tmp;
+                l.out = new HashSet<Integer>();
+                for(Integer s: l.succ){
+                    if(lines.get(s)!=null)l.out.addAll(lines.get(s).in);
+                    }
             }
+            for(Line l : lines.values()){
+                sent=sent&&l.in.equals(l.intag)&&l.out.equals(l.outtag);
+                // sent=sent||!l.out.equals(l.outtag);
+            }
+            if(sent)break;
+            if ((counting++)%10==0) {
+            System.out.println(counting);
+            }
+
+        }
+            System.out.println(lines.values());
+    }
+
+    public static void Leader() throws IOException, InterruptedException{
+        ArrayList<Integer> allMatches;
+        ArrayList<Line> lines = new ArrayList<Line>();
+        Pattern jalMatch = Pattern.compile("(?<=jal ).*");
+
+        Line curr;
+        String[] test = {" li Temp_1,100 ","  bge (Temp_1), -8($fp) "," j (Temp_3), -4($fp) "," jal Temp_2,Temp_3 "," sw Temp_2, -12($fp)","man:\n"};
+        String line;
+        String inputFile = "../FOLDER_5_OUTPUT/MIPS.txt";
+        // for(String s : test){
+        //     Matcher m = p2.matcher(s);
+        //     System.out.println(s+":"+m.find());
+            
+        // }
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
+        while ((line = bufferedReader.readLine()) != null) {
+            Pattern branchMatch = Pattern.compile("(beq|blt|ble|bgt|bge|bne|jr|j|jal)");
+            Pattern labelMatch = Pattern.compile(":$");
+            Matcher m = labelMatch.matcher(line);
+            System.out.println(line+":"+((m.find())?m.group():""));
+
         }
     }
+    
 }
+            // System.out.println(line+"----"+tmp[tmp.length-1]);
+            // System.out.println(labelMap.get(tmp[tmp.length-1]));
+            // System.out.println(tmp[tmp.length-1]);
+
+            // labelMap.put(tmp[tmp.length-1],linecount);
+            // leaderList.add(linecount+1);
+            // System.out.println(curr+" "+line);
+            // m = jalMatch.matcher(line);
+            // System.out.println(line+":"+((m.find())?m.group():""));
+            // m = jumpMatch.matcher(line);
+            // System.out.println(line+":"+((m.find())?m.group():""));
+            // System.out.println(line);
